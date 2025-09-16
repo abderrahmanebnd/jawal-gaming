1d2a1b814991d185e8a8649e6ce39341c0978657
Abderrahmane bnd <abdoubendaia7@gmail.com>
2025-09-16 10:51:22 +0100

    make the verify otp page not indexable

--- src/pages/login/VerifyOtp.jsx
+++ src/pages/login/VerifyOtp.jsx
@@ -3,6 +3,7 @@ import { useVerifyOtp } from "../../auth/useVerifyOtp";
 import { useResendOtp } from "../../auth/useResendOtp";
 import {RoutePaths} from "../../routes.js";
 import { Navigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 export default function VerifyOtp() {
   const [otp, setOtp] = useState(new Array(6).fill(""));
@@ -34,51 +35,57 @@ export default function VerifyOtp() {
     return <Navigate to={RoutePaths.login} replace />;
   }
   return (
-    <div className="otp-container">
-      <div className="otp-box">
-        <h2 className="otp-title">Verify OTP</h2>
-        <p className="otp-subtitle">
-          Enter the 6-digit code sent to your email
-        </p>
-
-        <form onSubmit={handleSubmit} className="otp-form">
-          <div className="otp-inputs">
-            {otp.map((digit, index) => (
-              <input
-                key={index}
-                id={`otp-input-${index}`}
-                type="text"
-                maxLength="1"
-                value={digit}
-                onChange={(e) => handleChange(e.target.value, index)}
-                className="otp-input"
-              />
-            ))}
-          </div>
-
-          <button type="submit" className="otp-button" disabled={isLoading}>
-            {isLoading ? "Verifying..." : "Verify OTP"}
-          </button>
-
-          <p className="otp-resend">
-            Didn’t get the code?{" "}
-            <button
-              type="button"
-              className="otp-resend-link"
-              disabled={isResending}
-              onClick={() =>
-                resendOtp(sessionStorage.getItem("email"), {
-                  onSuccess: () => {
-                    alert("OTP resent successfully!");
-                  },
-                })
-              }
-            >
-              {isResending ? "Resending..." : "Resend OTP"}
-            </button>
+    <>
+      <Helmet>
+        <meta name="robots" content="noindex, nofollow" />
+        <title>Jawal Gaming - Verify Otp</title>
+      </Helmet>
+      <div className="otp-container">
+        <div className="otp-box">
+          <h2 className="otp-title">Verify OTP</h2>
+          <p className="otp-subtitle">
+            Enter the 6-digit code sent to your email
           </p>
-        </form>
+
+          <form onSubmit={handleSubmit} className="otp-form">
+            <div className="otp-inputs">
+              {otp.map((digit, index) => (
+                <input
+                  key={index}
+                  id={`otp-input-${index}`}
+                  type="text"
+                  maxLength="1"
+                  value={digit}
+                  onChange={(e) => handleChange(e.target.value, index)}
+                  className="otp-input"
+                />
+              ))}
+            </div>
+
+            <button type="submit" className="otp-button" disabled={isLoading}>
+              {isLoading ? "Verifying..." : "Verify OTP"}
+            </button>
+
+            <p className="otp-resend">
+              Didn’t get the code?{" "}
+              <button
+                type="button"
+                className="otp-resend-link"
+                disabled={isResending}
+                onClick={() =>
+                  resendOtp(sessionStorage.getItem("email"), {
+                    onSuccess: () => {
+                      alert("OTP resent successfully!");
+                    },
+                  })
+                }
+              >
+                {isResending ? "Resending..." : "Resend OTP"}
+              </button>
+            </p>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
